95487730ea32558edab64413d4a5ee9c3d64eb4a
andreius20 <andreiuspus@gmail.com>
2022-05-24 15:31:45 +0300

    Updated the tests for Register and Login services in order to check if exceptions are thrown properly.

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -1,15 +1,63 @@
-import exception.GameAlreadyExistsException;
 import exception.UsernameAlreadyExistsException;
-import model.Developer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import services.DeveloperService;
-import services.GameService;
 import services.UserService;
 
 import java.io.IOException;
 
-class testClass {
+import static org.junit.jupiter.api.Assertions.*;
 
+
+public class testClass  {
+    @Test
+    void testLogin(){
+        try {
+            assertTrue(DeveloperService.addUser("Andy","alumnix"));
+            ///assertTrue(DeveloperService.addUser("Andy","cicici"));
+            ///UsernameAlreadyExistsException succesfullly thrown from Login!
+        } catch (UsernameAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+        try {
+            assertTrue(DeveloperService.loadDevelopersFromFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(DeveloperService.login("Andy","alumnix"));
+        /// if(DeveloperService.login("Andy","alumnix"))
+        ///  System.out.println("Login succesful");
+        ///else
+        /// System.out.println("Login failed");
+    }
+    @Test
+    void testDeveloperRegister() {
+        try {
+            assertTrue(DeveloperService.addUser("Robi","DND"));
+            /// assertTrue(DeveloperService.addUser("Robi","DND"));
+            ///UsernameAlreadyExistException succesfully thrown from DeveloperRegister!
+        } catch (UsernameAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+        try {
+            assertTrue(DeveloperService.loadDevelopersFromFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void testCustomerRegister() {
+        try {
+            assertTrue(UserService.addUser("Arrokuda","Cuda","customer"));
+            //// assertTrue(UserService.addUser("Arrokuda","Cuda","Customer"));
+            ///UsernameAlreadyExistsException  succesfully thrown for CustomerRegister!
+        } catch (UsernameAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+        try {
+            UserService.loadUsersFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-
